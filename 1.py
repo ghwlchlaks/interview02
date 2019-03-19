eb868397b1ce91d16a1dced88754a1878fa6750b
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-19 11:56:58 +0900

    변수명 변경

--- 1.py
+++ 1.py
@@ -2,24 +2,24 @@ num = int(input())
 
 
 # 1
-total_array = []
+answer_array = []
 for i in range(num):
     array = []
     for j in range(i+1):
         array.append(1)
-    total_array.append(array)
+    answer_array.append(array)
 
-for i in range(len(total_array)-1):
-    for j in range(len(total_array[i])):
+for i in range(len(answer_array)-1):
+    for j in range(len(answer_array[i])):
         if j == 0:
             continue
-        total_array[i+1][j] = total_array[i][j] + total_array[i][j-1]
+        answer_array[i+1][j] = answer_array[i][j] + answer_array[i][j-1]
 
 # 2
-total_array = [[1] * (i+1) for i in range(num)]
-for i in range(len(total_array)-1):
-    for j in range(1, len(total_array[i])):
-        total_array[i+1][j] = total_array[i][j] + total_array[i][j-1]
+answer_array = [[1] * (i+1) for i in range(num)]
+for i in range(len(answer_array)-1):
+    for j in range(1, len(answer_array[i])):
+        answer_array[i+1][j] = answer_array[i][j] + answer_array[i][j-1]
 
-print(total_array)
+print(answer_array)
 
